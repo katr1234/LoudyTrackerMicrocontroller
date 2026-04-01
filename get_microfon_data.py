ba310423c8f5a949fa690c083f09a4b6798a6f11
katr1234 <43642913+katr1234@users.noreply.github.com>
2026-04-01 10:31:51 +0200

    Function for measuring

--- get_microfon_data.py
+++ get_microfon_data.py
@@ -4,18 +4,26 @@ import numpy as np
 
 print("Start measuring... ")
 
-x = 0
 
-while(x < 100):
-	duration = 1  # Every second the microfon is measuring data
-
-	audio = sd.rec(int(duration * 44100), samplerate=44100, channels=1)
+def read_audio_data(duration, sample_rate, channels):
+	audio = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=channels)
 	sd.wait()
 
 	# Calculate dB
 	volume = np.linalg.norm(audio)
 
 	print("Volume: ", volume)
+
+
+
+
+duration = 1  # Every second the microfon is measuring data
+sample_rate = 44100  # in Hz
+channels = 1  # 1 --> Mono (1 Signal, everything will be recorded in one channel), 2 --> Stereo
+
+x = 0
+while(x < 2):
+	read_audio_data(duration, sample_rate, channels)	
 	x = x + 1
 
 
