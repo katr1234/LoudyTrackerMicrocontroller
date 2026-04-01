6d61a43eac0ae1e6aef59ade512f7878723e4a77
katr1234 <43642913+katr1234@users.noreply.github.com>
2026-04-01 12:25:28 +0200

    Add code for GPIO Pins

--- get_microfon_data.py
+++ get_microfon_data.py
@@ -2,10 +2,15 @@
 import sounddevice as sd
 import numpy as np
 import time
+import RPi.GPIO as GPIO
 
 print("Start measuring... ")
 
 DB_OFFSET = 80 # Synced offset value
+
+GPIO_RED = 17
+GPIO_YELLOW = 27
+GPIO_GREEN 22
 
 def read_audio_data(duration, sample_rate, channels):
 	## Step 1: Read Data
@@ -29,19 +34,48 @@ def read_audio_data(duration, sample_rate, channels):
 	print("RMS", rms)
 	print("DB: ", db)
 	
+	# GPIO
+	## Set/ Change Color
+	if db >= 85:
+		GPIO.output(GPIO_RED, True)
+		GPIO.output(GPIO_YELLOW, False)
+		GPIO.output(GPIO_GREEN, False)
+	else if db < 85 AND db >= 65:
+		GPIO.output(GPIO_RED, False)
+		GPIO.output(GPIO_YELLOW, True)
+		GPIO.output(GPIO_GREEN, False)
+	else db < 65:
+		GPIO.output(GPIO_RED, False)
+		GPIO.output(GPIO_YELLOW, False)
+		GPIO.output(GPIO_GREEN, True)
+	
 	# Sleep process
-	time.sleep(0.1)
-
-
+	time.sleep(2)
 
 duration = 1  # Every second the microfon is measuring data
 sample_rate = 44100  # in Hz
 channels = 1  # 1 --> Mono (1 Signal, everything will be recorded in one channel), 2 --> Stereo
 
 try:
+	# GPIO Pins
+	## GPIO Modus (BOARD/ BCM)
+	GPIO.setmode(GPIO.BCM)
+	
+	## Set Direction of the pins (IN/ OUT)
+	GPIO.setup(GPIO_RED, GPIO.OUT)
+	GPIO.setup(GPIO_YELLOW, GPIO.OUT)
+	GPIO.setup(GPIO_GREEN, GPIO.OUT)
+	
+	## Set Initial Color
+	GPIO.output(GPIO_RED, False)
+	GPIO.output(GPIO_YELLOW, False)
+	GPIO.output(GPIO_GREEN, True)
+	
 	while(True):
 		counter = 0
 		read_audio_data(duration, sample_rate, channels)	
 except KeyboardInterrupt:
 	print("Stopped")
+finally:
+	GPIO.cleanup()
 
