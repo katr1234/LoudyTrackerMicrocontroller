0146e2ad1f6642e5f84a4786001bd2b7a234d632
katr1234 <43642913+katr1234@users.noreply.github.com>
2026-04-01 11:20:06 +0200

    Add dB calculation

--- get_microfon_data.py
+++ get_microfon_data.py
@@ -3,17 +3,28 @@ import sounddevice as sd
 import numpy as np
 
 print("Start measuring... ")
-
+DB_OFFSET = 70 # Sample value 
 
 def read_audio_data(duration, sample_rate, channels):
-	audio = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=channels)
+	## Step 1: Read Data
+	audio = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=channels,  dtype="float32")
 	sd.wait()
 
 	# Calculate dB
-	volume = np.linalg.norm(audio)
+	## Step 2: Calculate RMS
+	rms = np.sqrt(np.mean(audio**2))
 
-	print("Volume: ", volume)
+	## Step 3: Calculate log
+	if rms > 1e-10:
+		dbfs = 20 * np.log10(rms)
+	else:
+		dbfs = -100
+		return
 
+	## Step 4: Calculate dB
+	db = dbfs + DB_OFFSET
+	
+	print("DB: ", db)
 
 
 
