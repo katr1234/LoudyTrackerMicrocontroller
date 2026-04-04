460e0fd1538fbd27da5b0a27dff8c003eac3a5f2
katr1234 <43642913+katr1234@users.noreply.github.com>
2026-04-04 13:38:33 +0200

    Add api call

--- microphone_trafficlight_controller.py
+++ microphone_trafficlight_controller.py
@@ -1,16 +1,23 @@
-# This part of the code records the microfon data and calculate the dB
 import sounddevice as sd
 import numpy as np
 import time
 import RPi.GPIO as GPIO
+import requests
+from datetime import datetime, timezone
+import asyncio
 
 print("Start measuring... ")
 
+# Calculating dB
 DB_OFFSET = 80 # Synced offset value
 
+# Traffic Light
 GPIO_RED = 17
 GPIO_YELLOW = 27
 GPIO_GREEN = 22
+
+# Send Data
+API_URL = 'http://127.0.0.1:8000/sensordata'
 
 def read_audio_data(duration, sample_rate, channels):
 	## Step 1: Read Data
@@ -30,16 +37,16 @@ def read_audio_data(duration, sample_rate, channels):
 	## Step 4: Calculate dB
 	db = dbfs + DB_OFFSET
 	
-	print("RMS", rms)
-	print("DB: ", db)
 	
 	# GPIO
 	## Set/ Change Color
-	if db >= 85:
+	# if db >= 85
+	if db >= 45:
 		GPIO.output(GPIO_RED, True)
 		GPIO.output(GPIO_YELLOW, False)
 		GPIO.output(GPIO_GREEN, False)
-	elif db < 85 and db >= 65:
+	# elif db < 85 and db >= 65:
+	elif db < 45 and db >= 35:
 		GPIO.output(GPIO_RED, False)
 		GPIO.output(GPIO_YELLOW, True)
 		GPIO.output(GPIO_GREEN, False)
@@ -47,9 +54,15 @@ def read_audio_data(duration, sample_rate, channels):
 		GPIO.output(GPIO_RED, False)
 		GPIO.output(GPIO_YELLOW, False)
 		GPIO.output(GPIO_GREEN, True)
+	return db
 	
-	# Sleep process
-	time.sleep(2)
+async def send_data(sensordata_payload):
+	try:
+		response = requests.post(API_URL, json=sensordata_payload)
+		print(response.text)
+	except:
+		print(f'Sending data {sensordata_payload} not possible. Data will not be sent agian');
+	
 
 duration = 1  # Every second the microfon is measuring data
 sample_rate = 44100  # in Hz
@@ -70,8 +83,24 @@ try:
 	GPIO.output(GPIO_YELLOW, False)
 	GPIO.output(GPIO_GREEN, True)
 	
+	sensordata_payload = []
+
 	while(True):
-		read_audio_data(duration, sample_rate, channels)	
+		count = 0
+		while(count < 5):
+			db_value = read_audio_data(duration, sample_rate, channels)	
+
+			sensordata_payload.append({
+				"timestamp": int(datetime.now(timezone.utc).timestamp()),
+				"value": float(db_value)
+			})
+			
+			# Sleep process
+			time.sleep(0.2)
+			count = count + 1			
+		
+		asyncio.run(send_data(sensordata_payload))	
+			
 except KeyboardInterrupt:
 	print("Stopped")
 finally:
