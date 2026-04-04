ab5a943a1697d2dc8a934f9fc1c95b3c296e42c1
katr1234 <43642913+katr1234@users.noreply.github.com>
2026-04-04 12:24:36 +0200

    Add test post request

--- test_send_data_to_api.py
+++ test_send_data_to_api.py
@@ -3,3 +3,30 @@ import requests
 x = requests.get('http://127.0.0.1:8000/version')
 
 print(x.text)
+
+url = 'http://127.0.0.1:8000/version'
+sensordata_payload = []
+sensordata_payload.append({
+    "timestamp": 1773990331,
+    "value": 60
+})
+sensordata_payload.append({
+    "timestamp": 1773990336,
+    "value": 63
+})
+
+sensordata_payload = [
+{
+    "timestamp": 1773990331,
+    "value": 60
+},
+{
+    "timestamp": 1773990336,
+    "value": 63
+}]
+
+
+x = request.post(url, json=sensordata_payload)
+print(x.text)
+                          
+  
